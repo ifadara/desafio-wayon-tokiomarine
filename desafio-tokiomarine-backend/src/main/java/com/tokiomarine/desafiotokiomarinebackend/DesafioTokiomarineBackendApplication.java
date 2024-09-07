b7e34a67fbfff23302b940bc059eff8b88b87b25
Icaro Silva <dsicejay@gmail.com>
2024-09-07 20:10:09 -0300

    feat: Start project

---
+++ desafio-tokiomarine-backend/src/main/java/com/tokiomarine/desafiotokiomarinebackend/DesafioTokiomarineBackendApplication.java
@@ -0,0 +1,13 @@
+package com.tokiomarine.desafiotokiomarinebackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesafioTokiomarineBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DesafioTokiomarineBackendApplication.class, args);
+	}
+
+}
---
+++ desafio-tokiomarine-backend/src/test/java/com/tokiomarine/desafiotokiomarinebackend/DesafioTokiomarineBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tokiomarine.desafiotokiomarinebackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesafioTokiomarineBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
